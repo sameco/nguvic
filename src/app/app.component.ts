491744ce73e7793090c9c84fae7a9a4068015d60
Eric Porcuna <sameco501@gmail.com>
2017-11-12 17:15:37 +0100

    Get and set Graus in RT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map';
+
+interface Grau {
+  ECTS: string;
+  assig: string;
+  codi: string;
+  coef: string;
+  credECTS: string;
+  credUVIC: string;
+  grup: string;
+  hores: string;
+  horesTot: string;
+  prof: string;
+  sem: string;
+  tipus: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +26,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  //Atributs Graus
+  ECTS: string;
+  assig: string;
+  codi: string;
+  coef: string;
+  credECTS: string;
+  credUVIC: string;
+  grup: string;
+  hores: string;
+  horesTot: string;
+  prof: string;
+  sem: string;
+  tipus: string;
+
+  grausCol: AngularFirestoreCollection<Grau>;
+  //done:string = "";
+  // graus: Observable<Grau[]>;
+  graus: any;
+  currentID: string;
+  currentGrau: Observable<Grau>;
+  grauDoc: AngularFirestoreDocument<Grau>;
+
+
+  //dtOptions: DataTables.Settings = {};
+  // We use this trigger because fetching the list of persons can be quite long,
+  // thus we ensure the data is fetched before rendering
+   //dtTrigger: Subject = new Subject();
+
+
+  addGrau() {
+    /*
+    AFEGIR DOCUMENT AMB ID PERSONALITZAT
+    this.afs.collection('bd/H0AcuFIYqTVItLjXjjHw/Graus').doc('custom-id').set({'assig': this.assig, 'codi': this.codi});
+    */
+    //TODO check assig or codi not null
+    this.afs.collection('bd/H0AcuFIYqTVItLjXjjHw/Graus').add({'assig': this.assig, 'codi': this.codi});
+  }
+
+  getGrau(id){
+    this.currentID = id;
+    this.grauDoc = this.afs.doc('bd/H0AcuFIYqTVItLjXjjHw/Graus/'+id);
+    this.currentGrau = this.grauDoc.valueChanges();
+  }
+
+  constructor(private afs: AngularFirestore){
+  }
+
+
+  ngOnInit(){
+    this.grausCol = this.afs.collection('bd/H0AcuFIYqTVItLjXjjHw/Graus');
+    // this.graus = this.grausCol.valueChanges();
+
+    this.graus = this.grausCol.snapshotChanges().map(actions =>{
+        /*if(this.done==""){
+          this.dtTrigger.next();
+          this.done="done";
+        }*/
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Grau;
+        const id = a.payload.doc.id;
+        return { id, data };
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+//Import all the required modules for the root component
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
+var firebaseConfig = {
+        apiKey: "AIzaSyAxiHYGYH7RcW8TpFrBeljWnbn1CgRPqPM",
+        authDomain: "gestor-de-graus.firebaseapp.com",
+        databaseURL: "https://gestor-de-graus.firebaseio.com",
+        projectId: "gestor-de-graus",
+        storageBucket: "gestor-de-graus.appspot.com",
+        messagingSenderId: "900967811321"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    DataTablesModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
